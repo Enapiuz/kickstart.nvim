cede52431d5680e370360d57db9e55b4c7e9fb6a
Vadim Shashin <git@enapiuz.com>
2024-10-12 18:51:42 +0200

    persistence + some keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,3 +8,8 @@ keymap.set('n', 'H', '^')
 keymap.set('n', 'J', '}')
 keymap.set('n', 'K', '{')
 keymap.set('n', 'L', '$')
+
+-- Buffers
+keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Delete buffer' })
+keymap.set('n', '<leader>bn', ':bn<CR>', { desc = 'Next buffer' })
+keymap.set('n', '<leader>bp', ':bp<CR>', { desc = 'Previous buffer' })
