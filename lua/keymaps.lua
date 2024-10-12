9de315fd9ac43ae4a4104020747e8a082d6f226e
Vadim Shashin <git@enapiuz.com>
2024-10-12 23:43:36 +0200

    comments and cmp

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,6 @@ local keymap = vim.keymap
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- Buffers
-keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Delete buffer' })
+keymap.set('n', '<leader>bd', ':bd!<CR>', { desc = 'Delete buffer' })
 keymap.set('n', '<leader>bn', ':bn<CR>', { desc = 'Next buffer' })
 keymap.set('n', '<leader>bb', ':bp<CR>', { desc = 'Previous buffer' })
