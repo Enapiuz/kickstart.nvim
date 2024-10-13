74097a40ab3df110eb1a073bb90d78720eb638da
Vadim Shashin <git@enapiuz.com>
2024-10-13 02:16:13 +0200

    separate colorscheme

--- init.lua
+++ init.lua
@@ -548,23 +548,6 @@ require('lazy').setup({
       },
     },
   },
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'lunacookies/vim-colors-xcode',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.defer_fn(function()
-        vim.cmd.colorscheme 'xcodedark'
-        vim.cmd.hi 'Comment gui=none'
-      end, 0)
-    end,
-  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
