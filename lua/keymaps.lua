e18ad606fd06a9a564b992d94e72d7379ea52a61
Vadim Shashin <git@enapiuz.com>
2024-10-13 15:23:36 +0200

    more keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,3 +7,7 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 keymap.set('n', '<leader>bd', ':bd!<CR>', { desc = 'Delete buffer' })
 keymap.set('n', '<leader>bn', ':bn<CR>', { desc = 'Next buffer' })
 keymap.set('n', '<leader>bb', ':bp<CR>', { desc = 'Previous buffer' })
+
+-- Lazy
+keymap.set('n', '<leader>ll', ':Lazy<CR>', { desc = 'Lazy' })
+keymap.set('n', '<leader>lm', ':Mason<CR>', { desc = 'Mason' })
