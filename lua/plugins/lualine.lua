97bd36b81fe0d9309297ad1dbb50f8c9514ca51c
Vadim Shashin <git@enapiuz.com>
2024-10-14 21:51:19 +0200

    transparent lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,10 +2,19 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local custom_theme = require 'lualine.themes.dracula'
+    custom_theme.normal.c.bg = 'NONE'
+    custom_theme.command.c.bg = 'NONE'
+    custom_theme.visual.c.bg = 'NONE'
+    custom_theme.inactive.c.bg = 'NONE'
+    custom_theme.insert.c.bg = 'NONE'
+    custom_theme.replace.c.bg = 'NONE'
+    -- custom_theme.terminal.c.bg = 'NONE'
+
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'everforest',
+        theme = custom_theme,
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' }, -- lualine v3
         disabled_filetypes = { 'NvimTree' },
