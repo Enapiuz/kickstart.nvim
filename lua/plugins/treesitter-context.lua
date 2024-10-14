a31f18a9f1e119d26357f79e4ffbf68d475bd215
Vadim Shashin <git@enapiuz.com>
2024-10-14 20:13:34 +0200

    transparent treesitter-context

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -2,6 +2,10 @@ return {
   'nvim-treesitter/nvim-treesitter-context',
   event = { 'BufReadPost', 'BufNewFile' },
   config = function()
-    require('treesitter-context').setup()
+    require('treesitter-context').setup {
+      mode = 'cursor',
+    }
+
+    vim.cmd 'highlight TreesitterContext guibg=NONE'
   end,
 }
