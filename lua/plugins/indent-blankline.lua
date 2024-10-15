5505c2cad75bb9e48ccc51fb108df292af51d6a8
Vadim Shashin <git@enapiuz.com>
2024-10-15 12:21:36 +0200

    better indents

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -14,20 +14,17 @@ return {
       }
 
       local hooks = require 'ibl.hooks'
+      local indentColor = '#444444'
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
-        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
-        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
-        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
-        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
-        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
-        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+        vim.api.nvim_set_hl(0, 'LightGrayIndent', { fg = indentColor })
+        vim.api.nvim_set_hl(0, 'IblScope', { fg = indentColor })
       end)
 
       require('ibl').setup {
         -- indent = { highlight = highlight },
         indent = {
-          char = '▏', -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+          char = '│', -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+          highlight = 'LightGrayIndent',
         },
         exclude = {
           filetypes = {
